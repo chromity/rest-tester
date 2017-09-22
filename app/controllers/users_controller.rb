81848bef509a5f81440990fc43d0341bd1a99af3
Michael Daryl Mayo <michaeldaryl.mayo@protonmail.com>
2017-09-22 19:38:07 +0800

    Completed controller method for GET requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,18 @@ class UsersController < ApplicationController
   end
 
   def show
-    @name = "I am the show action"
+    @name = "I am the Show action"
+  end
+
+  def create
+    @name = "I am the Create action"
+  end
+
+  def update 
+    @name = "I am the Update action"
+  end
+
+  def destroy
+    @name = "I am the Destroy action"
   end
 end
